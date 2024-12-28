787cbf250119f47249cd97fff6db3f920d16ec96
Demetrio Canade <dem.canade@gmail.com>
2024-12-28 13:47:52 +0100

    Modified files

--- Runtime/Utility/Utility.cs
+++ Runtime/Utility/Utility.cs
@@ -56,6 +56,7 @@ namespace DemGFramework.Utility
         typeof(AnimationCurve),
         typeof(AnimationClip),
         typeof(AudioClip),
+        typeof(AudioClip[])
         typeof(Color)
     };
 
